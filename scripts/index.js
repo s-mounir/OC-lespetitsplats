896d571d678dca0abb17d744c13ecb2889d42530
Sara Mounir <sara_mounir@hotmail.fr>
2023-05-24 10:54:16 +0200

    searchBar ingredients

--- scripts/index.js
+++ scripts/index.js
@@ -11,6 +11,7 @@ async function displayData(RecipeList) {
 
 async function init() {
   // Récupère les datas des recettes
+  console.log(recipes);
   displayData(recipes);
 }
 
@@ -19,6 +20,14 @@ init();
 const searchRecipe = document.querySelector('.searchBar');
 searchRecipe.addEventListener('input', (e) => {
   const element = e.target.value.toLowerCase();
-  const newRecipeList = recipes.filter((recipe) => recipe.name.toLowerCase().includes(element));
+  const recipeListName = recipes.filter((recipe) => recipe.name.toLowerCase().includes(element));
+  const recipeListIngredient = recipes.filter((recipe) =>
+    recipe.ingredients.filter((elem) =>
+      elem.ingredient.toLowerCase().includes(element))
+      .length > 0);
+  const recipeListDescription = recipes.filter((recipe) =>
+    recipe.description.toLowerCase().includes(element));
+  const concatList = recipeListName.concat(recipeListDescription).concat(recipeListIngredient);
+  const newRecipeList = concatList.filter((item, idx) => concatList.indexOf(item) === idx);
   displayData(newRecipeList);
 });
