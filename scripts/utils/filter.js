fb21a8d0f8bafb7767e2192e69637e34e838fe1e
Sara Mounir <sara_mounir@hotmail.fr>
2023-06-04 10:58:19 +0200

    filter AND not OR

--- scripts/utils/filter.js
+++ scripts/utils/filter.js
@@ -1,41 +1,53 @@
 function filter(searchInput, ingredientTags, appareilTags, ustensilTags) {
-  const ingredientFilter = (searchInput !== undefined && searchInput !== '') ? [...ingredientTags, searchInput] : ingredientTags;
+  const searchInputLC = searchInput.toLowerCase();
+  console.log(searchInputLC);
+
   // filtre des noms de recettes
   const recipeListName = recipes.filter((recipe) =>
-    recipe.name.toLowerCase().includes(searchInput));
+    recipe.name.toLowerCase().includes(searchInputLC));
+
+  // filtre des descriptions
+  const recipeListDescription = recipes.filter((recipe) =>
+    recipe.description.toLowerCase().includes(searchInputLC));
+
   // filtre des ingrédients
+  const recipesFiltered = recipes.filter((recipe) =>
+    recipe.ingredients.filter((elem) => elem.ingredient.toLowerCase().includes(searchInputLC))
+      .length > 0);
+
+  const concatList = recipeListName.concat(recipeListDescription).concat(recipesFiltered);
+
   let recipeListIngredient = [];
-  ingredientFilter.forEach((ingredient) => {
+  ingredientTags.forEach((ingredient) => {
     const ingredientLC = ingredient.toLowerCase();
-    const recipesFiltered = recipes.filter((recipe) =>
+    const recipesFiltered = concatList.filter((recipe) =>
       recipe.ingredients.filter((elem) => elem.ingredient.toLowerCase().includes(ingredientLC))
         .length > 0);
 
     recipeListIngredient = recipeListIngredient.concat(recipesFiltered);
   });
-  // filtre des descriptions
-  const recipeListDescription = recipes.filter((recipe) =>
-    recipe.description.toLowerCase().includes(searchInput));
+  const IngredientFilterResult = recipeListIngredient.length > 0 ? recipeListIngredient : concatList;
 
   // filtre des appareils
   let recipeListAppareil = [];
   appareilTags.forEach((appareil) => {
     const appareilLC = appareil.toLowerCase();
-    const recipesFiltered = recipes.filter((recipe) =>
+    const recipesFiltered = IngredientFilterResult.filter((recipe) =>
       recipe.appliance.toLowerCase().includes(appareilLC));
     recipeListAppareil = recipeListAppareil.concat(recipesFiltered);
   });
+  const AppareilFilterResult = recipeListAppareil.length > 0 ? recipeListAppareil : IngredientFilterResult;
+
   // filtre des ustensils
   let recipeListUstensil = [];
   ustensilTags.forEach((ustensil) => {
     const ustensilLC = ustensil.toLowerCase();
-    const recipesFiltered = recipes.filter((recipe) => recipe.ustensils.join().toLowerCase().includes(ustensilLC));
+    const recipesFiltered = AppareilFilterResult.filter((recipe) => recipe.ustensils.join().toLowerCase().includes(ustensilLC));
     recipeListUstensil = recipeListUstensil.concat(recipesFiltered);
   });
+  const UstensilFilterResult = recipeListUstensil.length > 0 ? recipeListUstensil : AppareilFilterResult;
 
-  const concatList = recipeListName.concat(recipeListDescription).concat(recipeListIngredient)
-    .concat(recipeListAppareil).concat(recipeListUstensil);
-  const newRecipeList = concatList.filter((item, idx) => concatList.indexOf(item) === idx);
+  const newRecipeList = UstensilFilterResult.filter((item, idx) => UstensilFilterResult.indexOf(item) === idx);
   displayData(newRecipeList);
   currentList = newRecipeList;
 }
